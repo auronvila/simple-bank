75b000d98e1c18832ad18d27ed0fcc425341eca0
auronvila <auronvila1121@gmail.com>
2024-12-23 19:08:44 +0100

    feat: added jwt maker and tests for jwt implementation

--- go.mod
+++ go.mod
@@ -4,9 +4,14 @@ go 1.23.2
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-playground/validator/v10 v10.23.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.10.0
+	go.uber.org/mock v0.5.0
+	golang.org/x/crypto v0.30.0
 )
 
 require (
@@ -20,7 +25,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -43,10 +47,8 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/mock v0.5.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.12.0 // indirect
-	golang.org/x/crypto v0.30.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.32.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
---
+++ token/jwt_maker_test.go
@@ -0,0 +1,60 @@
+package token
+
+import (
+	"github.com/auronvila/simple-bank/util"
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestJwtMaker(t *testing.T) {
+	maker, err := NewJwtMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+
+	duration := time.Minute
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.GenerateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, payload.IssuedAt, issuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredJwtToken(t *testing.T) {
+	maker, err := NewJwtMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.GenerateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.Nil(t, payload)
+}
+
+func TestInvalidJwtToken(t *testing.T) {
+	payload, err := NewPayload(util.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJwtMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.Nil(t, payload)
+}
